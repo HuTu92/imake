146b0e82ca1483dfda2a3746935800c7bb9a9191
Pavel <hanbragher@gmail.com>
2018-06-01 18:45:08 +0400

    add validator to image store

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,26 +4,30 @@ namespace imake\Http\Controllers;
 
 use Illuminate\Http\Request;
 use imake\Image;
+use Illuminate\Support\Facades\Validator;
 
 class ImageController extends Controller
 {
-    //
-    /**
-     * Store a new user.
-     *
-     * @param  Request  $request
-     * @return Response
-     */
+
+    protected function validator(array $data)
+    {
+        $rules = [
+            'images' => 'required|image',
+        ];
+        return Validator::make($data, $rules);
+    }
+
     public function store(Request $request)
     {
 
         if($request->hasFile("images")){
-            $image = Image::create( $request->file("images")[0]);
-            $image->save();
-            echo $image->file;
-        }else{
-            //TODO VALIDATE
-            echo "oops;";
+
+            $validator = $this->validator(['images'=>$request->file("images")[0]]);
+            if(!$validator->fails()) {
+                $image = Image::create($request->file("images")[0]);
+                $image->save();
+                echo $image->file;
+            }
         }
         exit;
     }
